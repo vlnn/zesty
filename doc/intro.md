120e80baea58ff2ed62639fd95df04df2db4f8ef
Volodymyr Anokhin <vlnn-github@proton.me>
2022-09-09 22:41:32 +0300

    Clojure init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to zesty
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/zesty/core_test.clj
@@ -0,0 +1,7 @@
+(ns zesty.core-test
+  (:require [clojure.test :refer :all]
+            [zesty.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
