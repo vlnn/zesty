9ddb3ac110a840e723ab68ced1e017362bfb662a
Volodymyr Anokhin <vlnn-github@proton.me>
2022-09-11 21:43:24 +0300

    Make search commands map

--- src/zesty/core.clj
+++ src/zesty/core.clj
@@ -2,18 +2,37 @@
   (:gen-class)
   (:require [clojure.string :as str]))
 
+(def asm-map
+  {:nop 0
+   :ld  "3E"})
+
+(defn is-valid?
+  [cmd]
+  true
+  )
+
 (defn parse-asm-line
-  "Parse and validate one line of asm code"
+  "Parse one line of asm code"
   [str]
-  (let [split (str/split str #" ")]
-    (first split)))
+  (let [split (str/split str #"(\s|,)+")]
+    (if is-valid? split)
+    (flatten  [(keyword (first split)) (rest split)])))
+
+(defn validate-parsed-command [cmd]
+  (is-valid? cmd))
+
+(defn get-code [mnemonic]
+  (get asm-map mnemonic))
+
+(defn compile-command
+  "Gets mnemonic with optional operands and returns machine code representation"
+  [cmd]
+  (get-code (first cmd)))
 
 (defn asm->hex
   "Return hex representation of asm mnemonic and arguments"
   [str]
-  (parse-asm-line str))
-
-
+  (compile-command (parse-asm-line str)))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/zesty/core_test.clj
+++ test/zesty/core_test.clj
@@ -4,11 +4,12 @@
 
 (deftest parse-the-line
   (testing "line parser"
-    (is (= "nop" (parse-asm-line "nop")))
-    (is (= "ld" (parse-asm-line "ld a,b")))
-    (is (= "inc" (parse-asm-line "inc a"))))
+    (is (= [:nop] (parse-asm-line "nop")))
+    (is (= [:ld "a" "b"] (parse-asm-line "ld a,b")))
+    (is (= [:inc "a"] (parse-asm-line "inc a"))))
   )
 
 (deftest one-byte-mnemonics-compile
   (testing "nop mnemonic works"
-    (is (= 0 (asm->hex "nop")))))
+    (is (= 0 (asm->hex "nop")))
+    (is (= 0 (compile-command '(:nop))))))
