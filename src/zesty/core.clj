050f40ed64f0340cd659435582fb1b10be1f6490
Volodymyr Anokhin <vlnn-github@proton.me>
2022-09-11 18:00:26 +0300

    Parse simplest mnemonic

--- src/zesty/core.clj
+++ src/zesty/core.clj
@@ -1,7 +1,21 @@
 (ns zesty.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]))
+
+(defn parse-asm-line
+  "Parse and validate one line of asm code"
+  [str]
+  (let [split (str/split str #" ")]
+    (first split)))
+
+(defn asm->hex
+  "Return hex representation of asm mnemonic and arguments"
+  [str]
+  (parse-asm-line str))
+
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (asm->hex "nop")))
--- test/zesty/core_test.clj
+++ test/zesty/core_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [zesty.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest parse-the-line
+  (testing "line parser"
+    (is (= "nop" (parse-asm-line "nop")))
+    (is (= "ld" (parse-asm-line "ld a,b")))
+    (is (= "inc" (parse-asm-line "inc a"))))
+  )
+
+(deftest one-byte-mnemonics-compile
+  (testing "nop mnemonic works"
+    (is (= 0 (asm->hex "nop")))))
